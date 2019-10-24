422e71fde1fbb93d607b1e9b2c1774eb3dcfa4cc
maxPrakken <maxprakken3@hotmail.nl>
2019-10-24 14:16:24 +0200

    fix all the things git wise

--- Concept/Powerup.cs
+++ Concept/Powerup.cs
@@ -24,7 +24,7 @@ namespace Concept
 
         public ScoreSwap()
         {
-            
+
         }
         public ScoreSwap(List<Player> pl)
         {
@@ -37,7 +37,7 @@ namespace Concept
         {
             int n = list.Count;
             while (n > 1)
-            {   
+            {
                 n--;
                 int k = rng.Next(n + 1);
                 T value = list[k];
@@ -51,7 +51,7 @@ namespace Concept
     {
         public ThemeSwap()
         {
-            
+
         }
     }
 
