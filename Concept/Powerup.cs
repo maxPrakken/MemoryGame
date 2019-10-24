1e5f94d9241268f2a4690d54a09bdb7f2700b354
ckerigon <ck-1992@hotmail.com>
2019-10-24 14:01:59 +0200

    quickfix

--- Concept/Powerup.cs
+++ Concept/Powerup.cs
@@ -37,7 +37,7 @@ namespace Concept
         {
             int n = list.Count;
             while (n > 1)
-            {
+            {   
                 n--;
                 int k = rng.Next(n + 1);
                 T value = list[k];
